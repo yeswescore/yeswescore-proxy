d6ac775df5272f03ee501828f72101ad2d7b43cb
yeswescore <dev@yeswescore.com>
2015-10-27 22:52:54 +0100

    patch proxy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 var fs = require('fs')
   , httpProxy = require('http-proxy');
 
+var http = require('http');
+http.globalAgent.maxSockets = 1000;
+
 if (fs.existsSync('../../yeswescore-api/server/conf.js')) {
   // configuration
   var Conf = require('../../yeswescore-api/server/conf.js');  
@@ -12,15 +15,24 @@ if (fs.existsSync('../../yeswescore-api/server/conf.js')) {
         host: Conf.get("proxy.http.targethost"),
         port: Conf.get("proxy.http.port.api.v1")
       });
-    } else {
+    }
+    else if (req.url.substr(0, 4) === "/v2/" || req.url.substr(0, 11) === '/bootstrap/') {
       console.log('routing ' + req.url + ' to api.v2 (port:' + Conf.get("proxy.http.port.api.default") +')');
       // routing /v2/* => to v2 server
       proxy.proxyRequest(req, res, {
         host: Conf.get("proxy.http.targethost"),
         port: Conf.get("proxy.http.port.api.default")
       });
+    } else {
+      console.log('routing ' + req.url + ' to local static server (port:' + Conf.get("www.http.static.port") +')');
+      // routing /v2/* => to v2 server
+      proxy.proxyRequest(req, res, {
+        host: Conf.get("www.http.proxy.targethost") || "localhost",
+        port: Conf.get("www.http.static.port") || "8080"
+      });
     }
   }).listen(Conf.get("proxy.http.port"));
+console.log('listening on port ' + Conf.get("proxy.http.port"));
 } else {
   // spawning blank error page
   var http = require('http')
